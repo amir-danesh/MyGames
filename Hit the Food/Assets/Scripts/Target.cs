7dff5d99c7c92be0a13488dddb41a2f280626ea2
A.Daneshpour <amirhosseindaneshpour@gmail.com>
2021-06-20 10:03:26 +0430

    New features and UI added
    UI created and 3 new game levels are added.

--- Hit the Food/Assets/Scripts/Target.cs
+++ Hit the Food/Assets/Scripts/Target.cs
@@ -38,13 +38,21 @@ public class Target : MonoBehaviour
     }
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
-        gameManager.UpdateScore(pointvalue);
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointvalue);
+        }
+        
     }
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
     }
 
     Vector3 RandomForce()
